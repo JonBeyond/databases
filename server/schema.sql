fd2d1e6f59a0b09ede9d6c4df9663176889ec8d1
JonBeyond <jonathan.mitzman@gmail.com>
2019-01-25 20:26:26 -0800

    "Completed schema.sql and copied client code"

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,39 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  /* Messages Table has two fk, room and user.*/
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  message CHAR(255),
+  createdAt BIGINT,
+  user INTEGER,
+  room INTEGER
 );
 
-/* Create other tables and define schemas for them here! */
 
 
 
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+		
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name CHAR(50)
+);
 
+
+
+
+		
+CREATE TABLE rooms (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name CHAR(50)
+);
+
+
+ALTER TABLE messages ADD FOREIGN KEY (user) REFERENCES users (id);
+ALTER TABLE messages ADD FOREIGN KEY (room) REFERENCES rooms (id);
